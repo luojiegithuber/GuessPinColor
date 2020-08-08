58022e48d89d223c0776ec3ff7f8f4cf007885a5
luojie <1015779635@qq.com>
2020-08-08 17:37:56 +0800

    解决聊天功能没法异步更新的bug

--- src/component/Chat/ChatDrawer.js
+++ src/component/Chat/ChatDrawer.js
@@ -2,11 +2,48 @@ import React , {useEffect, useState,useRef,useImperativeHandle} from 'react'
 import './ChatDrawer.css';
 import MsgRow from './MsgRow';
 import { Drawer, Button, Badge ,Input } from 'antd';
+//import SocketClient from './SocketClient'
+
+import io from 'socket.io-client'
+const socket = io('ws://localhost:3010');
+
+
+//监听服务器返回的消息
 
 const ChatDrawer = () => {
     const [visible, setVisible] = useState(false);
     const [msg, setMsg] = useState('');
     const [msgArr, setMsgArr] = useState([]);
+    const [count, setCounter] = useState(1)
+    const countRef = useRef(count);
+    const msgArrRef = useRef(msgArr);
+
+    
+    useEffect(() => {
+        //及时更新count值
+        countRef.current = count;  
+        msgArrRef.current = msgArr;  
+
+    });
+
+    useEffect(() => {
+        mounteMy();
+        return closeSocket;
+    }, []);
+
+    const mounteMy = () => {
+        console.log("触发mounteMy");
+        socket.on('recvMsgFromServer',function(data){
+            console.log('client receive :' , data);
+            addMsg(data);   // 本地添加 
+        })
+        
+    }
+
+    const closeSocket = () => {
+        console.log("关闭Socket");
+        socket.close();
+    }
 
     const { TextArea } = Input;
   
@@ -20,23 +57,26 @@ const ChatDrawer = () => {
         setVisible(false);
     };
 
+    
     //发送消息
     const sendMsg = () => {
-        console.log(msg);
-        addMsg(); 
-        setMsg(''); //清空
+        socket.emit('sendMsgToServer',msg);//发送给客户端
+        setMsg(''); // 清空
         document.getElementById('msg-chat-input').value = '';//清空
     }
 
     //添加消息
-    const addMsg = () => {
+    const addMsg = (thismsg) => {
+
+
         setMsgArr([
-            ...msgArr,
+            ...msgArrRef.current,
             {
-                msg: msg,
-                user: 'username'
+                msg: thismsg,
+                user: 'username',
             }
         ])
+
     }
 
     //读取信息
@@ -47,6 +87,7 @@ const ChatDrawer = () => {
   
     return (
       <div id="chat">
+
         <Badge count={5}>
             <Button type="primary" onClick={showDrawer}>
                 打开聊天室
@@ -64,11 +105,11 @@ const ChatDrawer = () => {
         >
             <div id="msg-panel">
                 {msgArr.map(obj => {
-                    return <MsgRow msgObj={obj} />
+                    return <MsgRow msgObj={obj}/>
                 })}
             </div>
             <br/>
-            <TextArea id="msg-chat-input" value={msg} onChange={setMymsg} autoSize={{minRows: 2, maxRows: 6}}></TextArea>
+            <TextArea id="msg-chat-input" value={msg} onChange={setMymsg}  autoSize={{minRows: 2, maxRows: 6}}></TextArea>
             <br/><br/>
             <Button type="primary" onClick={sendMsg} style={{float:'right'}}>
                 发送
